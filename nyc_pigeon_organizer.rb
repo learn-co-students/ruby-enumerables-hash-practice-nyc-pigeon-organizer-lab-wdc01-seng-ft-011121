dbd11194c2c2bdea1dcd8abcbfc5eb52adaef61e
Nyasha Perry <nyashaperry2005@yahoo.com>
2021-01-04 19:09:34 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,21 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  final_hash = {}
+  data.each do |key, value|
+    value.each do |final_value, names|
+      names.each do |name|
+        
+        if !final_hash[name]
+          final_hash[name] = {}
+        end
+
+        if !final_hash[name][key]
+          final_hash[name][key] = []
+        end
+
+        final_hash[name][key] << final_value.to_s
+
+      end
+    end
+  end
+  final_hash
 end
